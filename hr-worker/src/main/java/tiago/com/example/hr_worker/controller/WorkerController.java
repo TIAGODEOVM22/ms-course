20baecff3dd7811d5da1d9d183c6bd30b1a4dae4
Tiago de Oliveira <tiagodeoliveiravillalva@gmail.com>
2025-09-01 21:47:26 -0300

    feat(ribbon loading balancing)

--- hr-worker/src/main/java/tiago/com/example/hr_worker/controller/WorkerController.java
+++ hr-worker/src/main/java/tiago/com/example/hr_worker/controller/WorkerController.java
@@ -1,6 +1,9 @@
 package tiago.com.example.hr_worker.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,12 @@ import java.util.List;
 public class WorkerController {
 
     @Autowired
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
     private WorkerRepositories workerRepositories;
 
     @GetMapping
@@ -26,6 +35,7 @@ public class WorkerController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        logger.info("PORT = " + env.getProperty("local.server.port"));
         Worker obj = workerRepositories.findById(id).get();
         return ResponseEntity.ok(obj);
     }
