4eb4a7c89c8c60ac7cc9d0a251a48d47e603c932
Tiago de Oliveira <tiagodeoliveiravillalva@gmail.com>
2025-09-02 21:14:24 -0300

    feat (microserviços) configuring eureka clients

--- hr-payroll/src/main/java/tiago/com/example/hr_payroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/tiago/com/example/hr_payroll/HrPayrollApplication.java
@@ -2,11 +2,13 @@ package tiago.com.example.hr_payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.core.env.Environment;
 
-@RibbonClient(name = "hr-worker") /*sera cliente do hr-worker*/
+@EnableEurekaClient
+//@RibbonClient(name = "hr-worker") /*sera cliente do hr-worker*/
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
--- hr-worker/src/main/java/tiago/com/example/hr_worker/HrWorkerApplication.java
+++ hr-worker/src/main/java/tiago/com/example/hr_worker/HrWorkerApplication.java
@@ -2,7 +2,9 @@ package tiago.com.example.hr_worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
 
