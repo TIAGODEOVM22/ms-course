710adc0750229f0c3836d080e09208a88129c15a
Tiago de Oliveira <tiagodeoliveiravillalva@gmail.com>
2025-09-04 21:50:04 -0300

    feat: integrate Hystrix for circuit breaker support

--- hr-payroll/src/main/java/tiago/com/example/hr_payroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/tiago/com/example/hr_payroll/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package tiago.com.example.hr_payroll.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,15 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentFallback")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayments(@PathVariable Long workerId, @PathVariable Integer days) {
         Payment payment = paymentService.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentFallback(Long workerId, Integer days) {
+        Payment payment = new Payment("Brann", 400.0, days);
         return ResponseEntity.ok(payment);
     }
 
